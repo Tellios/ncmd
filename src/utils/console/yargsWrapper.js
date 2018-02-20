9dee0e163da47f72495edc29c002e9d7cdfc405b
tellios <sonny.johannesson@gmail.com>
2018-02-20 21:14:09 +0100

    Added alias command

--- src/utils/console/yargsWrapper.js
+++ src/utils/console/yargsWrapper.js
@@ -2,8 +2,13 @@
 
 const yargs = require('yargs');
 
-module.exports = () => {
-    return yargs
-        .strict()
+module.exports = (useStrict = true) => {
+    let wrapper = yargs
         .help();
+
+    if (useStrict) {
+        wrapper = yargs.strict();
+    }
+
+    return wrapper;
 }
