e95227d7dd532013c400c436b97c2e4edca35ab6
Sonny Johannesson <sonny.johannesson@sinch.com>
2019-09-11 16:41:30 +0200

    Fixed build issue

--- src/docker/utils/parseProcessRows.ts
+++ src/docker/utils/parseProcessRows.ts
@@ -64,7 +64,7 @@ export const parseProcessRows = (processRows: string[]): IProcessRow[] => {
             {}
         );
 
-        processes.push(process as unknown as IProcessRow);
+        processes.push((process as unknown) as IProcessRow);
     });
 
     return processes;
